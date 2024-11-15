fd2c317caf15844f48905b5ae37c8fec20785238
Md soheb alam <mohammedsohebalam@gmail.com>
2024-11-16 01:31:40 +0530

    port

--- backend-zip/src/server.js
+++ backend-zip/src/server.js
@@ -1,7 +1,7 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
 
-const port=process.env.PORT|| 4000;
+const port=process.env.PORT;
 app.listen(port,async ()=>{
     await connectDb()
     console.log("ecommerce api listing on port ",port)
